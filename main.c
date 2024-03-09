47730551f690e503768e9d2d15e59f5e10d54f64
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2024-03-09 11:39:53 -0300

    wip

--- main.c
+++ main.c
@@ -18,10 +18,16 @@
 #define TICK_TIMEOUT 0.3
 
 
-typedef struct Player {
+typedef struct PlayerNode {
   Vector2 position;
   Vector2 direction;
+} PlayerNode;
+
+typedef struct Player {
+  PlayerNode head;
+  PlayerNode* tail;
   int size;
+  int preallocated_tail;
 } PLAYER;
 
 Vector2 RandomPosition(){
@@ -46,13 +52,47 @@ Vector2 NewFoodLocation(Vector2* food, int food_count){
   return NewFoodLocation(food, food_count);
 }
 
-void Update(PLAYER* player, Vector2* food, int food_count){
-  player->position = Vector2Add(player->position, player->direction);
+void GrowPlayer(PLAYER* player){
+  if (player->size + 1 > player->preallocated_tail) {
+    PlayerNode* old_tail = player->tail;
+    int new_prellocated_tail = player->preallocated_tail+10;
+    player->tail = malloc(sizeof(PlayerNode)*new_prellocated_tail);
+    player->preallocated_tail = new_prellocated_tail;
+  }
+  player->tail[player->size - 1].position = Vector2Subtract(
+      player->tail[player->size - 2].position,
+      player->tail[player->size - 2].direction
+  );
+  player->size += 1;
+}
 
+void PropagateDirection(PLAYER* player) {
+  Vector2 propagating_direction = player->head.direction;
+  Vector2 swap_buffer;
+  for(int i = 0; i < player->size; i++){
+    swap_buffer = player->tail[i].direction;
+    player->tail[i].direction = propagating_direction;
+    propagating_direction = swap_buffer;
+  }
+}
+
+void CalculatePlayerMovement(PLAYER* player){
+  player->head.position = Vector2Add(player->head.position, player->head.direction);
+  for(int i = 0; i < player->size; i++){
+    player->tail[i].position = Vector2Add(
+      player->tail[i].position,
+      player->tail[i].direction
+    );
+  }
+  PropagateDirection(player);
+}
+
+void Update(PLAYER* player, Vector2* food, int food_count){
+  CalculatePlayerMovement(player);
   for(int i = 0; i < food_count; i++) {
-    if (player->position.x == food[i].x && player->position.y == food[i].y){
+    if (player->head.position.x == food[i].x && player->head.position.y == food[i].y){
       food[i] = NewFoodLocation(food, food_count);
-      player->size += 1;
+      GrowPlayer(player);
       printf("Player Size: %d\n", player->size);
     }
   }
@@ -61,12 +101,21 @@ void Update(PLAYER* player, Vector2* food, int food_count){
 
 void DrawPlayer(PLAYER* player){
   DrawRectangle(
-      GRID_BLOCK_WIDTH * player->position.x,
-      GRID_BLOCK_HEIGHT * player->position.y,
+      GRID_BLOCK_WIDTH * player->head.position.x,
+      GRID_BLOCK_HEIGHT * player->head.position.y,
       GRID_BLOCK_WIDTH,
       GRID_BLOCK_HEIGHT,
       GREEN
   );
+  for(int i = 0; i < player->size; i++){
+    DrawRectangle(
+        GRID_BLOCK_WIDTH * player->tail[i].position.x,
+        GRID_BLOCK_HEIGHT * player->tail[i].position.y,
+        GRID_BLOCK_WIDTH,
+        GRID_BLOCK_HEIGHT,
+        GREEN
+    );
+  }
 }
 
 void DrawFood(Vector2* food, int amount){
@@ -96,8 +145,13 @@ int main(int argc, char *argv[]){
   srand(time(NULL));
   float untilTick = TICK_TIMEOUT;
   PLAYER* player = malloc(sizeof(PLAYER));
-  player->position = (Vector2){PLAYER_STARTING_X, PLAYER_STARTING_Y};
-  player->direction = (Vector2){1,0};
+  player->head = (PlayerNode){
+    (Vector2){PLAYER_STARTING_X, PLAYER_STARTING_Y},
+    (Vector2){1,0}
+  };
+  player->preallocated_tail = 16;
+  player->size = 0;
+  player->tail = malloc(sizeof(PlayerNode)*player->preallocated_tail);
   Vector2* food = malloc(8 * sizeof(Vector2));
   for(int i = 0; i < 8; i++)
     food[i] = NewFoodLocation(food, 8);
@@ -112,16 +166,16 @@ int main(int argc, char *argv[]){
     untilTick -= GetFrameTime();
 
     // Handle Player Direction changes
-    if (IsKeyDown(KEY_A)){
-      player->direction = (Vector2){-1,0};
-    } else if (IsKeyDown(KEY_D)){
-      player->direction = (Vector2){1,0};
+    if (IsKeyDown(KEY_A) && player->head.direction.x == 0){
+      player->head.direction = (Vector2){-1,0};
+    } else if (IsKeyDown(KEY_D) && player->head.direction.x == 0){
+      player->head.direction = (Vector2){1,0};
     }
 
-    if (IsKeyDown(KEY_W)){
-      player->direction = (Vector2){0,-1};
-    } else if (IsKeyDown(KEY_S)) {
-      player->direction = (Vector2){0,1};
+    if (IsKeyDown(KEY_W) && player->head.direction.y == 0){
+      player->head.direction = (Vector2){0,-1};
+    } else if (IsKeyDown(KEY_S) && player->head.direction.y == 0) {
+      player->head.direction = (Vector2){0,1};
     }
 
     BeginDrawing();
